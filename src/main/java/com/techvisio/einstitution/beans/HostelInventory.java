3c6f71081f8303f3fb485cdfb3a1a50be64c33f2
nikhil <nikhil@192.168.2.4>
2015-01-07 22:42:20 +0530

    Hostel Work

---
+++ src/main/java/com/techvisio/einstitution/beans/HostelInventory.java
@@ -0,0 +1,41 @@
+package com.techvisio.einstitution.beans;
+
+public class HostelInventory {
+private String	typeCode; 
+private String	description;
+private int	threshold;
+private double	price;
+private int	roomCapacity;
+public String getTypeCode() {
+	return typeCode;
+}
+public void setTypeCode(String typeCode) {
+	this.typeCode = typeCode;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public int getThreshold() {
+	return threshold;
+}
+public void setThreshold(int threshold) {
+	this.threshold = threshold;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+public int getRoomCapacity() {
+	return roomCapacity;
+}
+public void setRoomCapacity(int roomCapacity) {
+	this.roomCapacity = roomCapacity;
+}
+
+
+}
---
+++ src/main/java/com/techvisio/einstitution/beans/RoomTypeDetail.java
@@ -0,0 +1,18 @@
+package com.techvisio.einstitution.beans;
+
+public class RoomTypeDetail {
+private String	roomNo ;
+private String	typeCode ;
+public String getRoomNo() {
+	return roomNo;
+}
+public void setRoomNo(String roomNo) {
+	this.roomNo = roomNo;
+}
+public String getTypeCode() {
+	return typeCode;
+}
+public void setTypeCode(String typeCode) {
+	this.typeCode = typeCode;
+}
+}
