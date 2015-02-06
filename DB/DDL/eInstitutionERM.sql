9d41d1a6377cdb7f767199d746697555731b537a
techvisio <techvisiosolutions@gmail.com>
2015-02-07 00:04:27 +0530

    DDLs

--- DB/DDL/eInstitutionERM.sql
+++ DB/DDL/eInstitutionERM.sql
@@ -1,387 +1,282 @@
-SET SESSION FOREIGN_KEY_CHECKS=0;
-
-/* Drop Tables */
-
-DROP TABLE IF EXISTS AcademicDetail;
-DROP TABLE IF EXISTS AddressDetail;
-DROP TABLE IF EXISTS AdmissionDiscountDtl;
-DROP TABLE IF EXISTS AdmissionInquiry;
-DROP TABLE IF EXISTS BranchPreference;
-DROP TABLE IF EXISTS QualificationSubjectDtl;
-DROP TABLE IF EXISTS StudentDetail;
-DROP TABLE IF EXISTS CasteCategoryMaster;
-DROP TABLE IF EXISTS ConsultantDetail;
-DROP TABLE IF EXISTS ConsultantMaster;
-DROP TABLE IF EXISTS ConsultantPaymentDetail;
-DROP TABLE IF EXISTS CounsellingMaster;
-DROP TABLE IF EXISTS CourseBranchMaster;
-DROP TABLE IF EXISTS CourseMaster;
-DROP TABLE IF EXISTS FeeHeadMaster;
-DROP TABLE IF EXISTS QualificationMaster;
-DROP TABLE IF EXISTS QuotaCodeMaster;
-DROP TABLE IF EXISTS StateMaster;
-DROP TABLE IF EXISTS SubjectMaster;
-
-
-
-
-/* Create Tables */
-
-CREATE TABLE AcademicDetail
-(
-	University varchar(100),
-	College_Name varchar(100),
-	Passing_Year varchar(20),
-	Percentage decimal,
-	Roll_No varchar(50),
-	Qualification_Id int NOT NULL,
-	File_No varchar(100) NOT NULL,
-	PRIMARY KEY (Qualification_Id, File_No)
+CREATE TABLE `castecategorymaster` (
+  `Id` int(11) NOT NULL,
+  `Category` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 );
 
-
-CREATE TABLE AddressDetail
-(
-	House_No int,
-	Locality varchar(50),
-	Landmark varchar(50),
-	District varchar(50),
-	City varchar(50),
-	Pincode int,
-	Address_Type char(1),
-	State_Id int NOT NULL,
-	File_No varchar(100) NOT NULL,
-	PRIMARY KEY (State_Id, File_No)
+CREATE TABLE `consultantmaster` (
+  `Id` bigint(20) NOT NULL,
+  `Name` varchar(50) DEFAULT NULL,
+  `Primary_Contact_No` varchar(50) DEFAULT NULL,
+  `Secondary_contact_No` varchar(50) DEFAULT NULL,
+  `Address` varchar(200) DEFAULT NULL,
+  `Email_Id` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 );
 
-
-CREATE TABLE AdmissionDiscountDtl
-(
-	FeeHead_Id int NOT NULL,
-	Amount decimal,
-	Percent decimal,
-	File_No varchar(100) NOT NULL,
-	PRIMARY KEY (FeeHead_Id, File_No)
+CREATE TABLE `counsellingmaster` (
+  `Id` bigint(20) NOT NULL,
+  `Counselling_Body` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 );
 
-
-CREATE TABLE AdmissionInquiry
-(
-	Inquiry_Id int NOT NULL,
-	Name varchar(100),
-	Father_Name varchar(100),
-	DOB date,
-	Contact_No varchar(20),
-	Application_Status varchar(50),
-	Due_Date date,
-	Created_By varchar(100),
-	Created_On date,
-	Updated_By varchar(100),
-	Updated_On date,
-	Intrested_Course_Id int NOT NULL,
-	Intrested_Branch_Id int NOT NULL,
-	FollowUp_Rquired bit(1),
-	PRIMARY KEY (Inquiry_Id)
+CREATE TABLE `coursemaster` (
+  `Id` int(11) NOT NULL,
+  `Course` varchar(50) DEFAULT NULL,
+  `Course_Type` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 );
 
-
-CREATE TABLE BranchPreference
-(
-	Branch_Preference_Id int,
-	File_No varchar(100) NOT NULL,
-	Course_Id int NOT NULL,
-	Branch_Id int NOT NULL,
-	PRIMARY KEY (File_No)
-);
-
-
-CREATE TABLE CasteCategoryMaster
-(
-	Id int NOT NULL,
-	Category varchar(100),
-	PRIMARY KEY (Id)
-);
-
-
-CREATE TABLE ConsultantDetail
-(
-	File_No varchar(100) NOT NULL,
-	Consultant_Id bigint NOT NULL,
-	Consultancy_Agreed varchar(10),
-	Payment_Mode varchar(50),
-	Amount_To_Pay decimal,
-	Due_Date date,
-	PRIMARY KEY (File_No, Consultant_Id)
-);
-
-
-CREATE TABLE ConsultantMaster
-(
-	Id bigint NOT NULL,
-	Name varchar(50),
-	Primary_Contact_No varchar(50),
-	Secondary_contact_No varchar(50),
-	Address varchar(200),
-	Email_Id varchar(100),
-	PRIMARY KEY (Id)
-);
-
-
-CREATE TABLE ConsultantPaymentDetail
-(
-	Amount decimal,
-	Pay_Date date,
-	File_No varchar(100) NOT NULL,
-	PRIMARY KEY (File_No)
-);
-
-
-CREATE TABLE CounsellingMaster
-(
-	Id bigint NOT NULL,
-	Counselling_Body varchar(100),
-	PRIMARY KEY (Id)
-);
-
-
-CREATE TABLE CourseBranchMaster
-(
-	Course_Id int NOT NULL,
-	Id int NOT NULL,
-	Branch varchar(100),
-	PRIMARY KEY (Course_Id, Id)
-);
-
-
-CREATE TABLE CourseMaster
-(
-	Id int NOT NULL,
-	Course varchar(50),
-	Course_Type varchar(50),
-	PRIMARY KEY (Id)
-);
-
-
-CREATE TABLE FeeHeadMaster
-(
-	Id int NOT NULL,
-	Fee_Head varchar(100),
-	PRIMARY KEY (Id)
-);
-
-
-CREATE TABLE QualificationMaster
-(
-	Id int NOT NULL,
-	QualifyingExam varchar(100),
-	PRIMARY KEY (Id)
-);
-
-
-CREATE TABLE QualificationSubjectDtl
-(
-	Subject_Id int NOT NULL,
-	Qualification_Id int NOT NULL,
-	Marks_Obtained decimal,
-	Max_Marks decimal,
-	File_No varchar(100) NOT NULL,
-	PRIMARY KEY (Subject_Id, File_No)
-);
-
-
-CREATE TABLE QuotaCodeMaster
-(
-	Id int NOT NULL,
-	Code varchar(50),
-	Description varchar(200),
-	PRIMARY KEY (Id)
-);
-
-
-CREATE TABLE StateMaster
-(
-	Id int NOT NULL,
-	State varchar(100),
-	PRIMARY KEY (Id)
-);
-
-
-CREATE TABLE StudentDetail
-(
-	File_No varchar(100) NOT NULL,
-	Enroll_No varchar(50) NOT NULL,
-	Uni_Enroll_No varchar(50),
-	Photo blob,
-	First_Name varchar(50),
-	Last_Name varchar(50),
-	Father_Name varchar(50),
-	Mother_Name varchar(50),
-	Gender varchar(20),
-	DOB varchar(30),
-	Blood_Group varchar(20),
-	Father_Occupation varchar(50),
-	FixedLine_No varchar(50),
-	Self_Mobile_No varchar(20),
-	Parent_Mobile_No varchar(20),
-	Gaurdian_Mobile_No varchar(50),
-	Email_Id varchar(50),
-	Gaurdian_Email_Id varchar(50),
-	Hostel bit(1),
-	Transportation bit(1),
-	Academic_Year int,
-	Semester int,
-	Management_Approval bit(1),
-	Fee_Paid bit(1),
-	Category_Id int NOT NULL,
-	Course_Id int NOT NULL,
-	Branch_Id int NOT NULL,
-	Created_By varchar(100),
-	Created_On date,
-	Updated_By varchar(100),
-	Updated_On date,
-	Fee_Received_By varchar(100),
-	Fee_Received_On date,
-	Document_Received_By varchar(100),
-	Document_Received_On date,
-	Document_verified_By varchar(100),
-	Document_Verified_On date,
-	Management_Approved_By varchar(100),
-	Management_Approved_On date,
-	Domicile_State_Id int NOT NULL,
-	PRIMARY KEY (File_No),
-	UNIQUE (Uni_Enroll_No)
-);
-
-
-CREATE TABLE SubjectMaster
-(
-	Subject_Id int NOT NULL,
-	Subject varchar(50),
-	PRIMARY KEY (Subject_Id)
+CREATE TABLE `coursebranchmaster` (
+  `Course_Id` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
+  `Branch` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `Course_Id` (`Course_Id`),
+  CONSTRAINT `coursebranchmaster_ibfk_1` FOREIGN KEY (`Course_Id`) REFERENCES `coursemaster` (`Id`)
 );
 
 
 
-/* Create Foreign Keys */
+CREATE TABLE `feeheadmaster` (
+  `Id` int(11) NOT NULL,
+  `Fee_Head` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+);
 
-ALTER TABLE StudentDetail
-	ADD FOREIGN KEY (Category_Id)
-	REFERENCES CasteCategoryMaster (Id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
+CREATE TABLE `qualificationmaster` (
+  `Id` int(11) NOT NULL,
+  `QualifyingExam` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+);
+
+CREATE TABLE `quotacodemaster` (
+  `Id` int(11) NOT NULL,
+  `Code` varchar(50) DEFAULT NULL,
+  `Description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ;
+
+CREATE TABLE `seq` (
+  `name` varchar(20) DEFAULT NULL,
+  `val` int(11) DEFAULT NULL
+) ;
+
+CREATE TABLE `statemaster` (
+  `Id` int(11) NOT NULL,
+  `State` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+);
+
+CREATE TABLE `subjectmaster` (
+  `Subject_Id` int(11) NOT NULL,
+  `Subject` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Subject_Id`)
+);
+
+CREATE TABLE `admissioninquiry` (
+  `Inquiry_Id` int(11) NOT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `Father_Name` varchar(100) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  `Contact_No` varchar(20) DEFAULT NULL,
+  `Application_Status` varchar(50) DEFAULT NULL,
+  `Due_Date` date DEFAULT NULL,
+  `Created_By` varchar(100) DEFAULT NULL,
+  `Created_On` date DEFAULT NULL,
+  `Updated_By` varchar(100) DEFAULT NULL,
+  `Updated_On` date DEFAULT NULL,
+  `Intrested_Branch_Id` int(11) NOT NULL,
+  `FollowUp_Rquired` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`Inquiry_Id`),
+  KEY `Intrested_Branch_Id` (`Intrested_Branch_Id`),
+  CONSTRAINT `admissioninquiry_ibfk_1` FOREIGN KEY (`Intrested_Branch_Id`) REFERENCES `coursebranchmaster` (`Id`)
+);
+
+CREATE TABLE `studentdetail` (
+  `File_No` varchar(100) NOT NULL,
+  `Enroll_No` varchar(50) DEFAULT NULL,
+  `Uni_Enroll_No` varchar(50) DEFAULT NULL,
+  `Photo` blob,
+  `First_Name` varchar(50) DEFAULT NULL,
+  `Last_Name` varchar(50) DEFAULT NULL,
+  `Father_Name` varchar(50) DEFAULT NULL,
+  `Mother_Name` varchar(50) DEFAULT NULL,
+  `Gender` varchar(20) DEFAULT NULL,
+  `DOB` varchar(30) DEFAULT NULL,
+  `Blood_Group` varchar(20) DEFAULT NULL,
+  `Father_Occupation` varchar(50) DEFAULT NULL,
+  `FixedLine_No` varchar(50) DEFAULT NULL,
+  `Self_Mobile_No` varchar(20) DEFAULT NULL,
+  `Parent_Mobile_No` varchar(20) DEFAULT NULL,
+  `Gaurdian_Mobile_No` varchar(50) DEFAULT NULL,
+  `Email_Id` varchar(50) DEFAULT NULL,
+  `Gaurdian_Email_Id` varchar(50) DEFAULT NULL,
+  `Hostel` bit(1) DEFAULT NULL,
+  `Transportation` bit(1) DEFAULT NULL,
+  `Academic_Year` int(11) DEFAULT NULL,
+  `Semester` int(11) DEFAULT NULL,
+  `Management_Approval` bit(1) DEFAULT NULL,
+  `Fee_Paid` bit(1) DEFAULT NULL,
+  `Category_Id` int(11) DEFAULT NULL,
+  `Branch_Id` int(11) DEFAULT NULL,
+  `Created_By` varchar(100) DEFAULT NULL,
+  `Created_On` date DEFAULT NULL,
+  `Updated_By` varchar(100) DEFAULT NULL,
+  `Updated_On` date DEFAULT NULL,
+  `Domicile_State_Id` int(11) DEFAULT NULL,
+  `Scholarship` bit(1) DEFAULT NULL,
+  `Remarks` varchar(500) DEFAULT NULL,
+  `Course_Id` int(11) DEFAULT NULL,
+  `Admission_Mode` varchar(100) DEFAULT NULL,
+  `Referred_By` varchar(100) DEFAULT NULL,
+  `Quota_Code` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`File_No`),
+  UNIQUE KEY `Uni_Enroll_No` (`Uni_Enroll_No`),
+  KEY `studentdetail_ibfk_4` (`Domicile_State_Id`),
+  KEY `studentdetail_ibfk_1` (`Category_Id`),
+  KEY `studentdetail_ibfk_2` (`Branch_Id`),
+  KEY `studentdetail_ibfk_3` (`Course_Id`),
+  CONSTRAINT `studentdetail_ibfk_1` FOREIGN KEY (`Category_Id`) REFERENCES `castecategorymaster` (`Id`),
+  CONSTRAINT `studentdetail_ibfk_2` FOREIGN KEY (`Branch_Id`) REFERENCES `coursebranchmaster` (`Id`),
+  CONSTRAINT `studentdetail_ibfk_3` FOREIGN KEY (`Course_Id`) REFERENCES `coursemaster` (`Id`),
+  CONSTRAINT `studentdetail_ibfk_4` FOREIGN KEY (`Domicile_State_Id`) REFERENCES `statemaster` (`Id`)
+);
+
+CREATE TABLE `addressdetail` (
+  `House_No` int(11) DEFAULT NULL,
+  `Locality` varchar(50) DEFAULT NULL,
+  `Landmark` varchar(50) DEFAULT NULL,
+  `District` varchar(50) DEFAULT NULL,
+  `City` varchar(50) DEFAULT NULL,
+  `Pincode` int(11) DEFAULT NULL,
+  `Address_Type` char(1) NOT NULL DEFAULT '',
+  `State_Id` int(11) NOT NULL,
+  `File_No` varchar(100) NOT NULL,
+  PRIMARY KEY (`File_No`,`Address_Type`),
+  KEY `addressdetail_ibfk_1` (`State_Id`),
+  CONSTRAINT `addressdetail_ibfk_1` FOREIGN KEY (`State_Id`) REFERENCES `statemaster` (`Id`),
+  CONSTRAINT `addressdetail_ibfk_2` FOREIGN KEY (`File_No`) REFERENCES `studentdetail` (`File_No`)
+);
 
 
-ALTER TABLE ConsultantDetail
-	ADD FOREIGN KEY (Consultant_Id)
-	REFERENCES ConsultantMaster (Id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
+CREATE TABLE `academicdetail` (
+  `University` varchar(100) DEFAULT NULL,
+  `College_Name` varchar(100) DEFAULT NULL,
+  `Passing_Year` varchar(20) DEFAULT NULL,
+  `Percentage` decimal(10,0) DEFAULT NULL,
+  `Roll_No` varchar(50) DEFAULT NULL,
+  `Qualification_Id` int(11) NOT NULL,
+  `File_No` varchar(100) NOT NULL,
+  PRIMARY KEY (`Qualification_Id`,`File_No`),
+  KEY `File_No` (`File_No`),
+  CONSTRAINT `academicdetail_ibfk_1` FOREIGN KEY (`File_No`) REFERENCES `studentdetail` (`File_No`)
+);
+
+CREATE TABLE `qualificationsubjectdtl` (
+  `Subject_Id` int(11) NOT NULL,
+  `Qualification_Id` int(11),
+  `Marks_Obtained` decimal(10,0) DEFAULT NULL,
+  `Max_Marks` decimal(10,0) DEFAULT NULL,
+  `File_No` varchar(100) NOT NULL,
+  PRIMARY KEY (`Subject_Id`,`File_No`),
+  KEY `Qualification_Id` (`Qualification_Id`),
+  KEY `File_No` (`File_No`),
+  CONSTRAINT `qualificationsubjectdtl_ibfk_1` FOREIGN KEY (`Qualification_Id`) REFERENCES `qualificationmaster` (`Id`),
+  CONSTRAINT `qualificationsubjectdtl_ibfk_2` FOREIGN KEY (`File_No`) REFERENCES `studentdetail` (`File_No`),
+  CONSTRAINT `qualificationsubjectdtl_ibfk_3` FOREIGN KEY (`Subject_Id`) REFERENCES `subjectmaster` (`Subject_Id`)
+) ;
+
+CREATE TABLE `admissiondiscountdtl` (
+  `FeeHead_Id` int(11) NOT NULL,
+  `Amount` decimal(10,0) DEFAULT NULL,
+  `Percent` decimal(10,0) DEFAULT NULL,
+  `File_No` varchar(100) NOT NULL,
+  PRIMARY KEY (`FeeHead_Id`,`File_No`),
+  KEY `File_No` (`File_No`),
+  CONSTRAINT `admissiondiscountdtl_ibfk_1` FOREIGN KEY (`File_No`) REFERENCES `studentdetail` (`File_No`)
+);
 
 
-ALTER TABLE StudentDetail
-	ADD FOREIGN KEY (Course_Id, Branch_Id)
-	REFERENCES CourseBranchMaster (Course_Id, Id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
+CREATE TABLE `branchpreference` (
+  `Branch_Preference_Id` int(11) DEFAULT NULL,
+  `File_No` varchar(100) NOT NULL,
+  `Branch_Id` int(11) NOT NULL,
+  PRIMARY KEY (`File_No`),
+  KEY `Branch_Id` (`Branch_Id`),
+  CONSTRAINT `branchpreference_ibfk_1` FOREIGN KEY (`Branch_Id`) REFERENCES `coursebranchmaster` (`Id`),
+  CONSTRAINT `branchpreference_ibfk_2` FOREIGN KEY (`File_No`) REFERENCES `studentdetail` (`File_No`)
+);
 
 
-ALTER TABLE AdmissionInquiry
-	ADD FOREIGN KEY (Intrested_Course_Id, Intrested_Branch_Id)
-	REFERENCES CourseBranchMaster (Course_Id, Id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
+
+CREATE TABLE `consultantdetail` (
+  `File_No` varchar(100) NOT NULL,
+  `Consultant_Id` bigint(20) NOT NULL,
+  `Consultancy_Agreed` varchar(10) DEFAULT NULL,
+  `Payment_Mode` varchar(50) DEFAULT NULL,
+  `Amount_To_Pay` decimal(10,0) DEFAULT NULL,
+  `Due_Date` date DEFAULT NULL,
+  PRIMARY KEY (`File_No`,`Consultant_Id`),
+  KEY `Consultant_Id` (`Consultant_Id`),
+  CONSTRAINT `consultantdetail_ibfk_1` FOREIGN KEY (`Consultant_Id`) REFERENCES `consultantmaster` (`Id`)
+);
 
 
-ALTER TABLE BranchPreference
-	ADD FOREIGN KEY (Course_Id, Branch_Id)
-	REFERENCES CourseBranchMaster (Course_Id, Id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
+CREATE TABLE `consultantpaymentdetail` (
+  `Amount` decimal(10,0) DEFAULT NULL,
+  `Pay_Date` date DEFAULT NULL,
+  `File_No` varchar(100) NOT NULL,
+  PRIMARY KEY (`File_No`)
+);
 
+CREATE TABLE `counsellingdetail` (
+  `File_No` varchar(200) DEFAULT NULL,
+  `Counselling_Id` bigint(20) DEFAULT NULL,
+  `Roll_No` varchar(100) DEFAULT NULL,
+  `Rank` bigint(20) DEFAULT NULL,
+  `Category_Rank` bigint(20) DEFAULT NULL,
+  `Percentile` decimal(10,0) DEFAULT NULL,
+  KEY `File_No` (`File_No`),
+  KEY `Counselling_Id` (`Counselling_Id`),
+  CONSTRAINT `counsellingdetail_ibfk_1` FOREIGN KEY (`File_No`) REFERENCES `studentdetail` (`File_No`),
+  CONSTRAINT `counsellingdetail_ibfk_2` FOREIGN KEY (`Counselling_Id`) REFERENCES `counsellingmaster` (`Id`)
+) ;
 
-ALTER TABLE CourseBranchMaster
-	ADD FOREIGN KEY (Course_Id)
-	REFERENCES CourseMaster (Id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
+ CREATE TABLE `transportmaster` (
+  `Route_Code` varchar(100) NOT NULL,
+  `Description` varchar(200) DEFAULT NULL,
+  `Threshold` varchar(200) DEFAULT NULL,
+  `Price` double DEFAULT NULL,
+  PRIMARY KEY (`Route_Code`)
+);
+ 
 
+CREATE TABLE `transportreservation` (
+  `File_No` varchar(100) NOT NULL,
+  `Fee_Paid` bit(1) DEFAULT NULL,
+  `Route_Code` varchar(100),
+  PRIMARY KEY (`File_No`),
+  KEY `Route_Code` (`Route_Code`),
+  CONSTRAINT `transportreservation_ibfk_1` FOREIGN KEY (`Route_Code`) REFERENCES `transportmaster` (`Route_Code`),
+  CONSTRAINT `transportreservation_ibfk_2` FOREIGN KEY (`File_No`) REFERENCES `studentdetail` (`File_No`)
+) ;
 
-ALTER TABLE QualificationSubjectDtl
-	ADD FOREIGN KEY (Qualification_Id)
-	REFERENCES QualificationMaster (Id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
+CREATE TABLE `transportallocation` (
+  `File_No` varchar(200) NOT NULL,
+  `Vehicle_Id` varchar(100),
+  PRIMARY KEY (`File_No`),
+  CONSTRAINT `transportallocation_ibfk_1` FOREIGN KEY (`File_No`) REFERENCES `studentdetail` (`File_No`)
+);
 
-
-ALTER TABLE AddressDetail
-	ADD FOREIGN KEY (State_Id)
-	REFERENCES StateMaster (Id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE StudentDetail
-	ADD FOREIGN KEY (Domicile_State_Id)
-	REFERENCES StateMaster (Id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE AddressDetail
-	ADD FOREIGN KEY (File_No)
-	REFERENCES StudentDetail (File_No)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE QualificationSubjectDtl
-	ADD FOREIGN KEY (File_No)
-	REFERENCES StudentDetail (File_No)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE AdmissionDiscountDtl
-	ADD FOREIGN KEY (File_No)
-	REFERENCES StudentDetail (File_No)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE AcademicDetail
-	ADD FOREIGN KEY (File_No)
-	REFERENCES StudentDetail (File_No)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE BranchPreference
-	ADD FOREIGN KEY (File_No)
-	REFERENCES StudentDetail (File_No)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE QualificationSubjectDtl
-	ADD FOREIGN KEY (Subject_Id)
-	REFERENCES SubjectMaster (Subject_Id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
+CREATE TABLE `vehicledetail` (
+  `Type` varchar(100) DEFAULT NULL,
+  `Capacity` varchar(200) DEFAULT NULL,
+  `Vehicle_No` varchar(200) DEFAULT NULL,
+  `Route_Code` varchar(100) NOT NULL,
+  `Vehicle_Id` varchar(100),
+  PRIMARY KEY (`Vehicle_Id`),
+  KEY `Route_Code` (`Route_Code`),
+  CONSTRAINT `vehicledetail_ibfk_1` FOREIGN KEY (`Route_Code`) REFERENCES `transportmaster` (`Route_Code`)
+); 
 
