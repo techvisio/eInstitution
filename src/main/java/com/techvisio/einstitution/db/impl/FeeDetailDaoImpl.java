96594bd88cea8ed472b7efe3978f1e9daec22c62
nikhil <nikhil@192.168.2.12>
2015-02-16 16:44:57 +0530

    Error clean..

--- src/main/java/com/techvisio/einstitution/db/impl/FeeDetailDaoImpl.java
+++ src/main/java/com/techvisio/einstitution/db/impl/FeeDetailDaoImpl.java
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 package com.techvisio.einstitution.db.impl;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -12,6 +10,7 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import com.techvisio.einstitution.beans.FeeDetail;
+import com.techvisio.einstitution.beans.FeeDiscountHead;
 import com.techvisio.einstitution.beans.FeeTransaction;
 import com.techvisio.einstitution.beans.StudentFeeStaging;
 import com.techvisio.einstitution.db.FeeDetailDao;
@@ -26,9 +25,7 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 		this.feeQueryProps = feeQueryProps;
 	}
 	
-	
-	
-	
+//	FeeDetail
 	public FeeDetail getFeeDetail(Long feeHeadId,Long course,Long branch) {
 		String getFeeDetailQuery=feeQueryProps.getProperty("getFeeDetailMaster");
 		SqlParameterSource namedParameter = new MapSqlParameterSource("FEE_HEAD_ID", feeHeadId)
@@ -53,9 +50,7 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 		
 	}
 	
-
-
-	public void addFeeDetail(FeeDetail feeDetail) {
+public void addFeeDetail(FeeDetail feeDetail) {
 		String addFeeDetailQuery=feeQueryProps.getProperty("addFeeDetailMaster");
 		SqlParameterSource namedParameter = new MapSqlParameterSource("FEE_HEAD_ID",feeDetail.getFeeHeadId())
 											.addValue("COURSE",feeDetail.getCourse())
@@ -66,9 +61,7 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 	}
 
 
-
-
-	public void updateFeeDetail(FeeDetail feeDetail) {
+public void updateFeeDetail(FeeDetail feeDetail) {
 		String updateFeeDetailQuery=feeQueryProps.getProperty("updateFeeDetailMaster");
 		SqlParameterSource namedParameter = new MapSqlParameterSource("FEE_HEAD_ID",feeDetail.getFeeHeadId())
 											.addValue("COURSE",feeDetail.getCourse())
@@ -79,9 +72,7 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 	}
 
 
-
-
-	public void deleteFeeDetail(Long feeHeadId,Long course,Long branch) {
+public void deleteFeeDetail(Long feeHeadId,Long course,Long branch) {
 		String deleteFeeDetailQuery=feeQueryProps.getProperty("deleteFeeDetailMaster");
 		SqlParameterSource namedParameter = new MapSqlParameterSource("FEE_HEAD_ID", feeHeadId)
 										.addValue("BRANCH", branch).addValue("COURSE", course);
@@ -90,10 +81,9 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 		
 	}
 
-	
+//	StudentFeeStaging
 
-
-	public StudentFeeStaging getStudentFeeStaging(String fileNo) {
+public StudentFeeStaging getStudentFeeStaging(String fileNo) {
 		String getQuery = feeQueryProps.getProperty("getStudentFeeStaging");
 		SqlParameterSource namedParameter = new MapSqlParameterSource("FILE_NO", fileNo);
 		
@@ -121,10 +111,7 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 	}
 
 	
-
-
-
-	public void addStudentFeeStaging(StudentFeeStaging studentFeeStaging) {
+public void addStudentFeeStaging(StudentFeeStaging studentFeeStaging) {
 		String addQuery = feeQueryProps.getProperty("addStudentFeeStaging");
 		SqlParameterSource namedParameter = new MapSqlParameterSource("FILE_NO", studentFeeStaging.getFileNo())
 											.addValue("COURSE", studentFeeStaging.getCourse())
@@ -134,10 +121,7 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 		getNamedParamJdbcTemplate().update(addQuery, namedParameter);
 	}
 
-
-
-
-	public void updateStudentFeeStaging(StudentFeeStaging studentFeeStaging) {
+public void updateStudentFeeStaging(StudentFeeStaging studentFeeStaging) {
 		String updateQuery = feeQueryProps.getProperty("updateStudentFeeStaging");
 		SqlParameterSource namedParameter = new MapSqlParameterSource("FILE_NO", studentFeeStaging.getFileNo())
 											.addValue("COURSE", studentFeeStaging.getCourse())
@@ -149,9 +133,7 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 	}
 
 
-
-
-	public void deleteStudentFeeStaging(String fileNo) {
+public void deleteStudentFeeStaging(String fileNo) {
 		String deleteQuery = feeQueryProps.getProperty("deleteStudentFeeStaging");
 		
 		SqlParameterSource namedParameter = new MapSqlParameterSource("FILE_NO", fileNo);
@@ -160,7 +142,7 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 	}
 
 
-
+//FeeTransaction
 	public FeeTransaction getFeeTransaction(String fileNo) {
 		String getQuery = feeQueryProps.getProperty("getFeeTransaction");
 		SqlParameterSource namedSqlParameter = new MapSqlParameterSource("File_no", fileNo);
@@ -188,8 +170,7 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 	}
 
 
-
-	public void addFeeTransaction(FeeTransaction feeTransaction) {
+public void addFeeTransaction(FeeTransaction feeTransaction) {
 		String addQuery = feeQueryProps.getProperty("addFeeTransaction");
 		
 		SqlParameterSource namedParameter = new MapSqlParameterSource("File_no", feeTransaction.getFileNo())
@@ -203,41 +184,9 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 		
 	}
 
+//FeeDiscountHead
 
-
-
-	
-	
-}
-=======
-package com.techvisio.einstitution.db.impl;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-
-import com.techvisio.einstitution.beans.FeeDetail;
-import com.techvisio.einstitution.beans.FeeDiscountHead;
-import com.techvisio.einstitution.beans.FeeTransaction;
-import com.techvisio.einstitution.beans.StudentFeeStaging;
-import com.techvisio.einstitution.db.FeeDetailDao;
-import com.techvisio.einstitution.util.CommonUtil;
-
-public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
-
-	private Properties feeQueryProps;
-
-	public void setFeeQueryProps(Properties feeQueryProps) {
-		this.feeQueryProps = feeQueryProps;
-	}
-
-	public FeeDiscountHead getfeeDiscountHead(Long headId) {
+public FeeDiscountHead getfeeDiscountHead(Long headId) {
 
 		String getQuery = feeQueryProps.getProperty("getFeeDiscountHead");
 
@@ -301,127 +250,5 @@ public class FeeDetailDaoImpl extends BaseDao implements FeeDetailDao{
 		getNamedParamJdbcTemplate().update(deleteQuery, namedParameter);
 	}
 
-	public List<FeeDetail> getFeeDetail() {
-		String getFeeDetailQuery=feeQueryProps.getProperty("getFeeDetailMaster");
-
-		List<FeeDetail> feeDetails =new ArrayList<FeeDetail>();
-
-		feeDetails = getNamedParamJdbcTemplate().query(getFeeDetailQuery, new RowMapper<FeeDetail>(){
-
-			public FeeDetail mapRow(ResultSet rs, int rowNum) throws SQLException {
-				FeeDetail detail = new FeeDetail();
-				detail.setBranch(CommonUtil.getLongValue(rs.getLong("BRANCH")));
-				detail.setCourse(CommonUtil.getLongValue(rs.getLong("COURSE")));
-				detail.setFeeAmount(rs.getDouble("FEE_AMOUNT"));
-				detail.setFeeHeadId(CommonUtil.getLongValue(rs.getLong("FEE_HEAD_ID")));
-				detail.setSemester(CommonUtil.getLongValue(rs.getLong("SEMESTER")));
-				return detail;
-			}
-
-		});
-
-		return feeDetails;
-	}
-
-	public StudentFeeStaging getStudentFeeStaging(String fileNo) {
-		String getQuery = feeQueryProps.getProperty("getStudentFeeStaging");
-		SqlParameterSource namedParameter = new MapSqlParameterSource("FILE_NO", fileNo);
-
-		List<StudentFeeStaging> feeStagings = getNamedParamJdbcTemplate().query(getQuery, namedParameter, new RowMapper<StudentFeeStaging>(){
-
-			public StudentFeeStaging mapRow(ResultSet rs, int rowNum)throws SQLException {
-				StudentFeeStaging feeStaging = new StudentFeeStaging();
-				feeStaging.setBranch(CommonUtil.getLongValue( rs.getInt("BRANCH")));
-				feeStaging.setCourse(CommonUtil.getLongValue(rs.getInt("COURSE")));
-				feeStaging.setFeeGenerated(rs.getBoolean("FEE_GENERATED"));
-				feeStaging.setFileNo(rs.getString("FILE_NO"));
-				feeStaging.setSemester(rs.getInt("SEMESTER"));
-
-				return feeStaging;
-			}
-
-		});
-
-		StudentFeeStaging feeStaging = null;
-		if(feeStagings != null && feeStagings.size()>0){
-			feeStaging = feeStagings.get(0);
-
-		}
-		return feeStaging;
-	}
-
-	public void addStudentFeeStaging(StudentFeeStaging studentFeeStaging) {
-		String addQuery = feeQueryProps.getProperty("addStudentFeeStaging");
-		SqlParameterSource namedParameter = new MapSqlParameterSource("FILE_NO", studentFeeStaging.getFileNo())
-		.addValue("COURSE", studentFeeStaging.getCourse())
-		.addValue("BRANCH", studentFeeStaging.getBranch())
-		.addValue("SEMESTER",studentFeeStaging.getSemester())
-		.addValue("FEE_GENERATED", studentFeeStaging.isFeeGenerated());
-		getNamedParamJdbcTemplate().update(addQuery, namedParameter);
-	}
-
-	public void updateStudentFeeStaging(StudentFeeStaging studentFeeStaging) {
-		String updateQuery = feeQueryProps.getProperty("updateStudentFeeStaging");
-		SqlParameterSource namedParameter = new MapSqlParameterSource("FILE_NO", studentFeeStaging.getFileNo())
-		.addValue("COURSE", studentFeeStaging.getCourse())
-		.addValue("BRANCH", studentFeeStaging.getBranch())
-		.addValue("SEMESTER",studentFeeStaging.getSemester())
-		.addValue("FEE_GENERATED", studentFeeStaging.isFeeGenerated());
-		getNamedParamJdbcTemplate().update(updateQuery, namedParameter);
-
-	}
-
-	public void deleteStudentFeeStaging(String fileNo) {
-		String deleteQuery = feeQueryProps.getProperty("deleteStudentFeeStaging");
-
-		SqlParameterSource namedParameter = new MapSqlParameterSource("FILE_NO", fileNo);
-
-		getNamedParamJdbcTemplate().update(deleteQuery, namedParameter);
-	}
-
-	public FeeTransaction getFeeTransaction(String fileNo) {
-		String getQuery = feeQueryProps.getProperty("getFeeTransaction");
-		SqlParameterSource namedSqlParameter = new MapSqlParameterSource("File_no", fileNo);
-
-		List<FeeTransaction> feeTransactions = getNamedParamJdbcTemplate().query(getQuery, namedSqlParameter, new RowMapper<FeeTransaction>(){
-
-			public FeeTransaction mapRow(ResultSet rs, int rowNum)throws SQLException {
-				FeeTransaction feeTransaction = new FeeTransaction();
-				feeTransaction.setAmount(rs.getDouble("AMOUNT"));
-				feeTransaction.setAmountTransactionType(rs.getString("AMOUNT_TRANSACTION_TYPE"));
-				feeTransaction.setDate(rs.getDate("DATE"));
-				feeTransaction.setFeeId(CommonUtil.getLongValue(rs.getLong("FEE_ID")));
-				feeTransaction.setFileNo(rs.getString("File_no"));
-				feeTransaction.setUser(rs.getString("USER"));
-				return feeTransaction;
-			}
-
-		});
-
-		FeeTransaction feeTransaction = null;
-		if(feeTransactions != null && feeTransactions.size()>0){
-			feeTransaction = feeTransactions.get(0);
-		}
-		return feeTransaction;
-	}
-
-	public void addFeeTransaction(FeeTransaction feeTransaction) {
-		String addQuery = feeQueryProps.getProperty("addFeeTransaction");
-
-		SqlParameterSource namedParameter = new MapSqlParameterSource("File_no", feeTransaction.getFileNo())
-		.addValue("FEE_ID", feeTransaction.getFeeId())
-		.addValue("DATE", feeTransaction.getDate())
-		.addValue("USER", feeTransaction.getUser())
-		.addValue("AMOUNT", feeTransaction.getAmount())
-		.addValue("AMOUNT_TRANSACTION_TYPE", feeTransaction.getAmountTransactionType());
-
-		getNamedParamJdbcTemplate().update(addQuery, namedParameter);
-
-	}
-
-
-
-
 
 }
->>>>>>> branch 'master' of https://github.com/techvisio/eInstitution.git
--- src/test/java/com/techvisio/einstitution/db/FeeDetailDaoTest.java
+++ src/test/java/com/techvisio/einstitution/db/FeeDetailDaoTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.techvisio.einstitution.db;
 
 import org.junit.Test;
@@ -8,6 +8,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.techvisio.einstitution.beans.FeeDetail;
+import com.techvisio.einstitution.beans.FeeDiscountHead;
 import com.techvisio.einstitution.beans.FeeTransaction;
 import com.techvisio.einstitution.beans.StudentFeeStaging;
 
@@ -118,88 +119,7 @@ public class FeeDetailDaoTest {
 	public void testDeleteStudentFeeStaging(){
 		dao.deleteStudentFeeStaging("1");
 	}
-}
-=======
-package com.techvisio.einstitution.db;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.techvisio.einstitution.beans.FeeDiscountHead;
-import com.techvisio.einstitution.beans.FeeTransaction;
-import com.techvisio.einstitution.beans.StudentFeeStaging;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:spring-config/Application-context.xml"})
-
-public class FeeDetailDaoTest {
-	
-	@Autowired
-	FeeDetailDao dao;
-	
-	@Test
-	public void testGetFeeDetail(){
-		System.out.println("Data is :-"+dao.getFeeDetail());
-	}
-	
-	@Test
-	
-	public void testAddStudentFeeStaging(){
-		StudentFeeStaging feeStaging = new StudentFeeStaging();
-		feeStaging.setBranch(1L);
-		feeStaging.setCourse(1L);
-		feeStaging.setFeeGenerated(false);
-		feeStaging.setFileNo("1");
-		feeStaging.setSemester(1);
-		
-		dao.addStudentFeeStaging(feeStaging);
-	}
-	
-	@Test
-	public void testGetStudentFeeStaging(){
-		StudentFeeStaging feeStaging = dao.getStudentFeeStaging("1");
-		System.out.println("Data is :- "+feeStaging);
-	}
-	
-	@Test
-	public void testUpdateStudentFeeStaging(){
-		StudentFeeStaging feeStaging = new StudentFeeStaging();
-		feeStaging.setBranch(4L);
-		feeStaging.setCourse(2L);
-		feeStaging.setFeeGenerated(true);
-		feeStaging.setFileNo("1");
-		feeStaging.setSemester(2);
-		
-		dao.updateStudentFeeStaging(feeStaging);
-	}
-	
-	
-	@Test
-	public void testAddFeeTransaction(){
-		FeeTransaction feeTransaction = new FeeTransaction();
-		feeTransaction.setAmount(1000.0);
-		feeTransaction.setAmountTransactionType("Debit");
-		feeTransaction.setDate(null);
-		feeTransaction.setFeeId(2L);
-		feeTransaction.setFileNo("1");
-		feeTransaction.setUser("ABC");
-		
-		dao.addFeeTransaction(feeTransaction);
-	}
-	
-	@Test
-	public void testGetFeeTransaction(){
-		FeeTransaction feeTransaction = dao.getFeeTransaction("1");
-		System.out.println("Data is :- "+feeTransaction);
-	}
-	
-	@Test
-	public void testDeleteStudentFeeStaging(){
-		dao.deleteStudentFeeStaging("1");
-	}
 
     @Test
     public void testFeeDiscountHead(){
@@ -228,4 +148,3 @@ public class FeeDetailDaoTest {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/techvisio/eInstitution.git
