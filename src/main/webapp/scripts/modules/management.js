c2e73638e63647dce51c16537410a062cd096bb5
pgusain <pgusain@pgusain-PC>
2015-03-12 12:23:02 -0700

    management js

--- src/main/webapp/scripts/modules/management.js
+++ src/main/webapp/scripts/modules/management.js
@@ -3,6 +3,14 @@ var managementModule = angular.module('managementModule', ['ui.bootstrap']);
 managementModule.controller('managementController',['$scope','managementService', function($scope,managementService) {
 	$scope.unapprovedRecords=[];
 	$scope.currentFetchLimit=20;
+	$scope.form.fileNo="";
+	$scope.managementData={};
+	
+	$scope.getDetails = function(){
+		$scope.form.fileNo=prompt("Provide file no","");
+		$scope.getAdmissionDetail();
+	};
+	
 	$scope.getUnapprovedRecords=function(){
 		managementService.getUnapprovedList($scope.currentFetchLimit)
 		.then(function(response) {
@@ -16,13 +24,26 @@ managementModule.controller('managementController',['$scope','managementService'
 				 })
 	};
 	
+	$scope.getAdmissionDetail=function(){
+		managementService.getAdmissionDetail($scope.form.fileNo)
+		.then(function(response) {
+					 console.log('Data received from service : ');
+					 console.log(response);
+					 if (response != null && response.data != null && response.data.responseBody != null) {
+						 $scope.managementData = response.data.responseBody;
+					 } else {
+						 console.log(response.data.error);
+					 }
+				 })
+	};
 }]);
 
 managementModule.service("managementService", function($http, $q) {
 
 	 // Return public API.
 	 return ({
-		 getUnapprovedList : getUnapprovedList
+		 getUnapprovedList : getUnapprovedList,
+		 getAdmissionDetail: getAdmissionDetail
 	 });
 
 	 function getUnapprovedList(size) {
@@ -31,6 +52,21 @@ managementModule.service("managementService", function($http, $q) {
 		 var request = $http({
 			 method : "get",
 			 url : "admission/uapprovedList/"+size,
+			 params : ""
+
+		 });
+
+		 return (request.then(handleSuccess, handleError));
+
+	 }
+	 
+	 function getAdmissionDetail(fileNo)
+	 {
+
+		 console.log('Get admission detail service');
+		 var request = $http({
+			 method : "get",
+			 url : "management/admission/approval/"+fileNo,
 			 params : ""
 
 		 });
