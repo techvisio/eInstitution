073ecefe2968247293c21fcee2775e57a039d4b2
sgusain <sgusain91@gmail.com>
2015-03-17 22:34:20 +0530

    management service work (17 mar, 2015)

--- src/main/webapp/scripts/modules/management.js
+++ src/main/webapp/scripts/modules/management.js
@@ -1,8 +1,24 @@
 var managementModule = angular.module('managementModule', ['ui.bootstrap']);
 
 managementModule.controller('managementController',['$scope','managementService', function($scope,managementService) {
+	
 	$scope.unapprovedRecords=[];
 	$scope.currentFetchLimit=20;
+	$scope.feeTransactionAdmissionBean = {};
+	
+	$scope.getPreviousBalace = function(){
+		var fileNo = $scope.feeTransactionAdmissionBean.basicInfo.fileNo;
+		managementService.getPreviousBalace(fileNo).then(function(response) {
+			 console.log('get privious balance data received from service : ');
+			 console.log(response);
+			 if (response != null && response.data != null && response.data.responseBody != null) {
+				 $scope.feeTransactionAdmissionBean = response.data.responseBody;
+			 } else {
+				 console.log(response.data.error);
+			 }
+		 })
+};
+	
 	$scope.getUnapprovedRecords=function(){
 		managementService.getUnapprovedList($scope.currentFetchLimit)
 		.then(function(response) {
@@ -22,7 +38,8 @@ managementModule.service("managementService", function($http, $q) {
 
 	 // Return public API.
 	 return ({
-		 getUnapprovedList : getUnapprovedList
+		 getUnapprovedList : getUnapprovedList,
+		 getPreviousBalace : getPreviousBalace
 	 });
 
 	 function getUnapprovedList(size) {
@@ -31,6 +48,21 @@ managementModule.service("managementService", function($http, $q) {
 		 var request = $http({
 			 method : "get",
 			 url : "admission/uapprovedList/"+size,
+			 params : ""
+
+		 });
+
+		 return (request.then(handleSuccess, handleError));
+
+	 }
+
+	 
+	 function getPreviousBalace(fileNo) {
+
+		 console.log('getting previous balance in service');
+		 var request = $http({
+			 method : "get",
+			 url : "fee/feeTransaction/"+fileNo,
 			 params : ""
 
 		 });
