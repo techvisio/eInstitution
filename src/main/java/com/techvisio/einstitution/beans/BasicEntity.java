e0c316ab77f25a3c808e677ff4dc97397f6d48bb
btry <btry@192.168.2.7>
2015-06-16 15:47:26 +0530

    BasicEntity bean (16 June,2015)

---
+++ src/main/java/com/techvisio/einstitution/beans/BasicEntity.java
@@ -0,0 +1,49 @@
+package com.techvisio.einstitution.beans;
+
+import java.util.Date;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+public class BasicEntity {
+
+	private String createdBy;
+	private String updatedBy;
+	private Date createdOn;
+	private Date updatedOn;
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	public Date getUpdatedOn() {
+		return updatedOn;
+	}
+	public void setUpdatedOn(Date updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+	
+	@PrePersist
+	  protected void onCreate() {
+	    createdOn = new Date();
+	  }
+
+	  @PreUpdate
+	  protected void onUpdate() {
+	    updatedOn = new Date();
+	  }
+	
+}
